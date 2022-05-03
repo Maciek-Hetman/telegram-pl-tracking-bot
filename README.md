00d32750c13850f316586d2361a3f64787ac9049
Maciek-Hetman <66074902+Maciek-Hetman@users.noreply.github.com>
2022-05-03 13:36:07 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Telegram tracking bot
+This is telegram bot for tracking parcels in Poland.  
+Currently supported carriers:  
+    - InPost
+    - Poczta Polska
+    - idk maybe dhl, dpd and ups will work too, didn't test it yet
+
+# Usage
+Project is still in heavy development.  
+Currently available commands:  
+/track (tracking number) (carrier)  
+/help  
+/start  
+
--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def help(update, context):
 def error(update, context):
     logger.warning('Update %s caused error "%s"' % (update, context.error))
 
+# /track command
 def track(update, context):
     if len(context.args) == 2:
         tracking_number = context.args[0]
@@ -52,7 +53,6 @@ def main(BOT_KEY):
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("track", track))
     dp.add_error_handler(error)
-
 
     updater.start_polling()
 
