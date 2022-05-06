a0fb77b7c40f7559112dfcc219b7a73e4a19e6ad
Maciek-Hetman <maciekhetman577@gmail.com>
2022-05-06 12:06:57 +0200

    match functions in dhl api

--- TrackDHLParcel.py
+++ TrackDHLParcel.py
@@ -50,7 +50,23 @@ class TrackDHLParcel(object):
         header = {'DHL-API-Key': api_key}
         req_url = self.BASE_URL + "?trackingNumber=" + tracking_number
         
-        self.req = requests.get(req_url, headers=header).json()
+        self.parcel = requests.get(req_url, headers=header).json()
     
+    def get_tracking_details(self, index):
+        return self.parcel['shipments']['events'][index]['description']
 
+    def get_current_status(self):
+        return self.parcel['shipments']['status']['description']
+    
+    def get_tracking_history(self):
+        history = []
+
+        for i in range(0, len(self.parcel['shipments']['events'])-1):
+            history.append(self.get_tracking_details(i))
         
+        pretty_text = ""
+
+        for entry in history:
+            pretty_text = pretty_text + entry['description'] + "\n"
+        
+        return pretty_text
